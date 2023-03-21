0426e04c463b08cb20a02d931172da534b6a124d
sohail ahmed <sohailahmedlaghari21@gmail.com>
2023-03-21 14:01:53 +0500

    Initial commit

---
+++ contracts/b.sol
@@ -0,0 +1,20 @@
+// pragma solidity ^0.8.11;
+
+// contract ExampleDapp {
+//     string dapp_name; // state variable
+
+//     // Called when the contract is deployed and initializes the value
+//     constructor() public {
+//         dapp_name = "My Example dapp";
+//     }
+
+//     // Get Function
+//     function read_name() public view returns(string) {
+//         return dapp_name;
+//     }
+
+//     // Set Function
+//     function update_name(Books") public {
+//         dapp_name = value;
+//     }
+// }
